5b063b3fd81d734ceb11b761286a0f683ba60c64
Minat0_ <minat0_@outlook.com>
2022-05-02 22:31:44 +0300

    docs: Add javadoc to DynmapSimpleClans#getLang

--- src/main/java/net/sacredlabyrinth/phaed/dynmap/simpleclans/DynmapSimpleClans.java
+++ src/main/java/net/sacredlabyrinth/phaed/dynmap/simpleclans/DynmapSimpleClans.java
@@ -1,16 +1,5 @@
 package net.sacredlabyrinth.phaed.dynmap.simpleclans;
 
-import java.io.File;
-import java.util.Objects;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
-import org.bukkit.ChatColor;
-import org.bukkit.plugin.Plugin;
-import org.bukkit.plugin.java.JavaPlugin;
-import org.dynmap.DynmapAPI;
-import org.dynmap.markers.MarkerAPI;
-
 import net.sacredlabyrinth.phaed.dynmap.simpleclans.layers.ClanHomes;
 import net.sacredlabyrinth.phaed.dynmap.simpleclans.layers.Kills;
 import net.sacredlabyrinth.phaed.dynmap.simpleclans.managers.CommandManager;
@@ -18,6 +7,17 @@ import net.sacredlabyrinth.phaed.dynmap.simpleclans.managers.PlayerManager;
 import net.sacredlabyrinth.phaed.simpleclans.SimpleClans;
 import net.sacredlabyrinth.phaed.simpleclans.managers.ClanManager;
 import net.sacredlabyrinth.phaed.simpleclans.managers.SettingsManager;
+import org.bukkit.ChatColor;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.dynmap.DynmapAPI;
+import org.dynmap.markers.MarkerAPI;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.File;
+import java.util.Objects;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class DynmapSimpleClans extends JavaPlugin {
 	private static DynmapSimpleClans instance;
@@ -116,66 +116,64 @@ public class DynmapSimpleClans extends JavaPlugin {
 		}
 
 		severe("Cannot find SimpleClans!");
-	}
+    }
 
-	private void initApis() {
-		markerApi = dynmapApi.getMarkerAPI();
+    private void initApis() {
+        markerApi = dynmapApi.getMarkerAPI();
 
-		if (markerApi == null) {
-			severe("Error loading Dynmap marker API!");
-		}
-	}
+        if (markerApi == null) {
+            severe("Error loading Dynmap marker API!");
+        }
+    }
 
-	public static void info(String msg) {
-		log.log(Level.INFO, LOG_PREFIX + msg);
-	}
+    public static void info(@NotNull String msg) {
+        log.log(Level.INFO, LOG_PREFIX + msg);
+    }
 
-	public static void severe(String msg) {
-		log.log(Level.SEVERE, LOG_PREFIX + msg);
-	}
+    public static void severe(@NotNull String msg) {
+        log.log(Level.SEVERE, LOG_PREFIX + msg);
+    }
 
-	public static DynmapSimpleClans getInstance() {
-		return instance;
-	}
+    public static DynmapSimpleClans getInstance() {
+        return instance;
+    }
 
-	public MarkerAPI getMarkerApi() {
-		return markerApi;
-	}
+    public MarkerAPI getMarkerApi() {
+        return markerApi;
+    }
 
-	public ClanManager getClanManager() {
+    public ClanManager getClanManager() {
 		return simpleclansCore.getClanManager();
 	}
 
 	public DynmapAPI getDynmapApi() {
 		return dynmapApi;
-	}
+    }
 
-	public ClanHomes getClanHomes() {
-		return clanHomes;
-	}
+    public ClanHomes getClanHomes() {
+        return clanHomes;
+    }
 
-	public Kills getKills() {
-		return kills;
-	}
+    public Kills getKills() {
+        return kills;
+    }
 
-	public PlayerManager getPlayerManager() {
-		return playerManager;
-	}
+    public PlayerManager getPlayerManager() {
+        return playerManager;
+    }
 
-	public SettingsManager getSimpleClansSettingsManager() {
-		return simpleclansCore.getSettingsManager();
-	}
-	
-	public String getLang(String lang) {
-		String msg = null;
-		if (lang != null) {
-			msg = getConfig().getString("language." + lang);
-		}
-		if (msg == null) {
-			msg = "Missing language for " + lang;
-		} else {
-			msg = ChatColor.translateAlternateColorCodes('&', msg);
-		}
-		return msg;
-	}
+    public SettingsManager getSettingsManager() {
+        return simpleclansCore.getSettingsManager();
+    }
+
+    /**
+     * @param messageKey The key
+     * @return Translated colored message from key
+     */
+    public String getLang(@NotNull String messageKey) {
+        String msg = getConfig().getString("language." + messageKey);
+        return msg == null ?
+                String.format("Missing language for %s key", messageKey) :
+                ChatColor.translateAlternateColorCodes('&', msg);
+    }
 }
