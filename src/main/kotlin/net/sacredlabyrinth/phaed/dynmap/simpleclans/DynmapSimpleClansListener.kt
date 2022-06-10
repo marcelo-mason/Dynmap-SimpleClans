cde3fdd5e2c8d8aaa6f1b4f965b18b9dece0eed5
Minat0_ <minat0_@outlook.com>
2022-06-10 14:08:35 +0300

    refactor: DRY on DynmapSimpleClansListener

--- src/main/kotlin/net/sacredlabyrinth/phaed/dynmap/simpleclans/DynmapSimpleClansListener.kt
+++ src/main/kotlin/net/sacredlabyrinth/phaed/dynmap/simpleclans/DynmapSimpleClansListener.kt
@@ -1,5 +1,6 @@
 package net.sacredlabyrinth.phaed.dynmap.simpleclans
 
+import net.sacredlabyrinth.phaed.simpleclans.Clan
 import net.sacredlabyrinth.phaed.simpleclans.Kill
 import net.sacredlabyrinth.phaed.simpleclans.events.*
 import org.bukkit.Bukkit.getScheduler
@@ -11,41 +12,32 @@ import java.time.LocalDateTime
 class DynmapSimpleClansListener(val plugin: DynmapSimpleClans) : Listener {
 
     @EventHandler(priority = MONITOR, ignoreCancelled = true)
-    fun onSetHome(event: PlayerHomeSetEvent) {
-        val clan = event.clan
-        // Running the runnable on next tick to get the actual clan home location
-        getScheduler().runTask(plugin, Runnable {
-            plugin.homeLayer?.upsertMarker(clan)
-            plugin.landsLayer?.upsertMarker(clan)
-        })
-    }
+    fun onSetHome(event: PlayerHomeSetEvent) = upsertMarkers(event.clan)
 
     @EventHandler(priority = MONITOR, ignoreCancelled = true)
-    fun onHomeClear(event: PlayerHomeClearEvent) {
-        val tag = event.clan.tag
-        plugin.homeLayer?.markerSet?.findMarker(tag)?.deleteMarker()
-        plugin.landsLayer?.markerSet?.findMarker(tag)?.deleteMarker()
-    }
+    fun onHomeClear(event: PlayerHomeClearEvent) = deleteMarkers(event.clan.tag)
 
     @EventHandler(priority = MONITOR, ignoreCancelled = true)
-    fun onDisband(event: DisbandClanEvent) {
-        val tag = event.clan.tag
-        plugin.homeLayer?.markerSet?.findMarker(tag)?.deleteMarker()
-        plugin.landsLayer?.markerSet?.findMarker(tag)?.deleteMarker()
-    }
+    fun onDisband(event: DisbandClanEvent) = deleteMarkers(event.clan.tag)
 
     @EventHandler(priority = MONITOR, ignoreCancelled = true)
-    fun onModtag(event: TagChangeEvent) {
-        val clan = event.clan
-        // Running the runnable on next tick to get the actual clan tag color
-        getScheduler().runTask(plugin, Runnable {
-            plugin.landsLayer?.upsertMarker(clan)
-            plugin.homeLayer?.upsertMarker(clan)
-        })
-    }
+    fun onModtag(event: TagChangeEvent) = upsertMarkers(event.clan)
 
     @EventHandler(priority = MONITOR, ignoreCancelled = true)
     fun onKill(event: AddKillEvent) =
         plugin.killsLayer?.createMarker(Kill(event.attacker, event.victim, LocalDateTime.now()))
+
+    private fun deleteMarkers(clanTag: String) {
+        plugin.homeLayer?.markerSet?.findMarker(clanTag)?.deleteMarker()
+        plugin.landsLayer?.markerSet?.findAreaMarker(clanTag)?.deleteMarker()
+    }
+
+    private fun upsertMarkers(clan: Clan) {
+        // Running the runnable on next tick to get the actual clan data
+        getScheduler().runTask(plugin, Runnable {
+            plugin.homeLayer?.upsertMarker(clan)
+            plugin.landsLayer?.upsertMarker(clan)
+        })
+    }
 }
 
